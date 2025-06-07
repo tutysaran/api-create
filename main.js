79bfee22fb6e63b9b4291c622a1d5fc5a70dd048
saran <selvakumarraj432@gmail.com>
2025-06-07 11:23:38 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ app.get('/', (req, res) => {
     res.json({ msg: "Hello, World!" });
 });
 
+//CRUD FUNCTIONALITY OF MOVIES
+
+// R -- for Reading
+app.get('/movies',()=>{
+    
+})
+
+// C -- for Creating
+app.post('/movies',()=>{
+
+})
+
+// U -- for Updating
+app.put('/movies/:id',()=>{
+})
+
+// D -- for Deleting
+app.delete('/movies/:id',()=>{
+
+})
+
+
+// Start the server
+
 app.listen(Port,()=>{
     console.log(`The server is running at http://localhost:${Port}`);
 
