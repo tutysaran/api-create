f07289d0daeacb0bbe766148d732fab84d39ad23
saran <selvakumarraj432@gmail.com>
2025-06-07 11:10:28 +0530

    feat:app structure

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import  express from 'express';
+
+const app = express();
+const Port=6969;
+app.get('/', (req, res) => {
+    res.json({ msg: "Hello, World!" });
+});
+
+app.listen(Port,()=>{
+    console.log(`The server is running at http://localhost:${Port}`);
+
+});
